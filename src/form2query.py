6273414eb911ca27cb30b02122395fdf334f09e9
George Kanellopoulos <gkanel97@hotmail.com>
2020-06-10 19:14:16 +0300

    Added code for DELETE SQL queries

--- src/form2query.py
+++ src/form2query.py
@@ -7,9 +7,14 @@ def insert(form_fields, field_type, table):
     field_arr = []
     value_arr = []
     type_arr = []
+    query_arr = []
 
     for field,type in zip(form_fields, field_type):
-        new_data = request.form.get("insert_" + field)
+        if field == 'reg_date':
+            new_data = str(date.today())
+        else:
+            new_data = request.form.get("insert_" + field)
+
         if new_data != "" and new_data != None:
             field_arr.append(field)
             value_arr.append(new_data)
@@ -30,7 +35,13 @@ def insert(form_fields, field_type, table):
         value_tuple += value_arr[-1] + ")"
         
     query_str = "INSERT INTO " + table + " " + field_tuple + " VALUES " + value_tuple + ";"
-    return [query_str]
+
+    query_arr.append(query_str)
+    if table == 'Product':
+        new_price = request.form.get("insert_current_price")
+        query_arr.append("INSERT INTO Price (barcode, start_date, amount) VALUES ({}, '{}', {})".format(value_arr[0], str(date.today()), new_price))
+
+    return query_arr
 
 def update(form_fields, field_type, table):
 
@@ -41,7 +52,7 @@ def update(form_fields, field_type, table):
 
     for field,type in zip(form_fields, field_type):
         new_data = request.form.get("insert_" + field)
-        if new_data != "" and new_data != 'None':
+        if new_data != "" and new_data != None:
             field_arr.append(field)
             value_arr.append(new_data)
             type_arr.append(type)
@@ -74,5 +85,29 @@ def update(form_fields, field_type, table):
         if old_price != new_price: 
             query_arr.append("UPDATE Price SET end_date = '{}' WHERE barcode = {} AND amount = {}".format(str(date.today()), value_arr[0], old_price))
             query_arr.append("INSERT INTO Price (barcode, start_date, amount) VALUES ({}, '{}', {})".format(value_arr[0], str(date.today()), new_price))
+
+    return query_arr
+
+def delete(table):
+
+    query_arr = []
+    if table == 'Customer':
+        selected_card_id = request.form.get("insert_card_id")
+        query_arr.append("UPDATE Transaction SET card_id = NULL WHERE card_id = {}".format(selected_card_id))
+        query_arr.append("DELETE FROM Customer WHERE card_id = {}".format(selected_card_id))
+
+    elif table == 'Product':
+        selected_barcode = request.form.get("insert_barcode")
+        old_price = query.get_one_col("SELECT current_price FROM Product WHERE barcode = {}".format(selected_barcode))[0]
+        query_arr.append("UPDATE Price SET end_date = '{}' WHERE barcode = {} AND amount = {}".format(str(date.today()), selected_barcode, old_price))
+        query_arr.append("INSERT INTO Price (barcode, start_date) VALUES ({}, '{}')".format(selected_barcode, str(date.today()))) 
+        query_arr.append("UPDATE Product SET current_price = NULL WHERE barcode = {}".format(selected_barcode))
+
+    elif table == 'Store':
+        selected_store_id = request.form.get("insert_store_id")
+        query_arr.append("DELETE FROM buy_products WHERE transaction_id IN (SELECT transaction_id FROM Transaction WHERE store_id = {})".format(selected_store_id))
+        query_arr.append("DELETE FROM Transaction WHERE store_id = {}".format(selected_store_id))
+        query_arr.append("DELETE FROM offers_products WHERE store_id = {}".format(selected_store_id))
+        query_arr.append("DELETE FROM Store WHERE store_id = {}".format(selected_store_id))
 
     return query_arr
